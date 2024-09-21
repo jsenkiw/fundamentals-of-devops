f63f22b66a9fe870d0f48546b4d141e10c1ffc61
John Senkiw <john.senkiw@systal-ts.com>
2024-09-21 18:01:27 +0100

    Part 3 Server Orchestration Complete

--- ch3/ansible/roles/sample-app/files/app.js
+++ ch3/ansible/roles/sample-app/files/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n\nChapter3: Deployed by Ansible'); (1)
+  res.end('Hello, World!\n\nFundamentals of DevOps!\n\nChapter3: Deployed by Ansible'); (1)
 });
 
 const port = 8080; (2)
